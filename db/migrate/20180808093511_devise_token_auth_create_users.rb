6afcb331e619474093d0d7175b297fc95bdf2a53
andrey.b <bogdanovskiya@gmail.com>
2018-08-13 10:23:08 +0300

    fix device_token_auth_migration for existing users

--- db/migrate/20180808093511_devise_token_auth_create_users.rb
+++ db/migrate/20180808093511_devise_token_auth_create_users.rb
@@ -46,6 +46,15 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       ## Tokens
       t.text :tokens
 
+      # updates the user table immediately with the above defaults
+      User.reset_column_information
+      # finds all existing users and updates them.
+      # if you change the default values above you'll also have to change them here below:
+      User.find_each do |user|
+        user.uid = user.email
+        user.provider = 'email'
+        user.save!
+      end
     end
 
     add_index :users, :email,                unique: true
